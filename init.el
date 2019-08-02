45a4f6ec4205f9cfb1208c72e26735bc4444876f
Gordon Brown <gordon.brown@elastic.co>
2019-08-02 10:52:30 -0600

    Update the autogen'd part

--- init.el
+++ init.el
@@ -229,10 +229,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(electric-pair-mode t)
  '(package-selected-packages
    (quote
-    (evil-cleverparens evil-lispy smartparens evil-lisp-state es-mode markdown-mode adoc-mode ace-jump-mode slime geiser evil-magit magit rainbow-delimiters neotree all-the-icons projectile general which-key helm doom-themes evil use-package))))
+    (evil-cleverparens es-mode markdown-mode adoc-mode ace-jump-mode slime geiser evil-magit magit rainbow-delimiters neotree all-the-icons projectile general which-key helm doom-themes evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
