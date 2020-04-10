9739d29ebb1eec4a17109a335d3b8725d5a8847e
Gordon Brown <gordon.brown@elastic.co>
2020-04-10 17:21:59 -0600

    Auto-init racket xp mode

--- init.el
+++ init.el
@@ -195,7 +195,9 @@
 (use-package racket-mode
   :ensure t
   :init
-  (setq racket-program "/usr/local/bin/racket")
+  (progn
+    (setq racket-program "/usr/local/bin/racket")
+    (add-hook 'racket-mode-hook #'racket-xp-mode))
   :config
   (general-define-key
    :states '(normal visual insert emacs)
@@ -323,7 +325,12 @@
  '(package-selected-packages
    (quote
     (racket-mode which-key use-package sly rainbow-delimiters projectile parinfer neotree markdown-mode helm general geiser faceup evil-magit evil-collection es-mode doom-themes all-the-icons adoc-mode ace-jump-mode))))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; custom-set-faces was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
 ;; Your init file should contain only one such instance.
